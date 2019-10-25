4be704cceef23ac916e9b4ee0d98af6a09ed3a91
Ricky Chen <chenricky@gmail.com>
2019-10-25 15:50:54 -0700

    add js and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+var images = ["./assets/images/bootstrap.png", "./assets/images/github-logo.jpg", ".assets//images/logo_JavaScript.png"];
+var showImage;
+var count = 0;
+
+$("#start").click(startSlideshow);
+
+function displayImage() {
+    $("#image-holder").html("<img src=" + images[count] + " width='400px'>");
+  }
+
+  function nextImage() {
+    //  TODO: Increment the count by 1.
+    count++;
+  
+    // TODO: Show the loading gif in the "image-holder" div.
+    $("#image-holder").html("<img src='./assets/images/loading.gif' width='200px'/>");
+  
+    
+    // TODO: If the count is the same as the length of the image array, reset the count to 0.
+    if (count === images.length) {
+      count = 0;
+    }
+    // TODO: Use a setTimeout to run displayImage after 1 second.
+    setTimeout(displayImage, 1000);
+  
+  }
+  function startSlideshow() {
+
+    // TODO: Use showImage to hold the setInterval to run nextImage.
+    showImage = setInterval(nextImage, 3000);
+  
+  }
+  
+  function stopSlideshow() {
+  
+    clearInterval(showImage);
+  
+  }
+  
+  // need to add a countdown timer fucntion
+  displayImage();
